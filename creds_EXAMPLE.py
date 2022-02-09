129a6a31244cca669d192599098c43ad29553084
altanner <8190834+altanner@users.noreply.github.com>
2022-02-09 20:52:46 +0000

    upping an example of what creds.py looks like

---
+++ creds_EXAMPLE.py
@@ -0,0 +1,9 @@
+uname = "yourusername"
+passw = "yourpassword"
+
+#~ pretend we are a browser
+user_agent = {'User-agent': 'Mozilla/5.0'}
+
+#~ this session cookie will change each time,
+#~ get from browser after login
+cookies = {"PHPSESSID":"biglongmessofnumbersandletters"}
