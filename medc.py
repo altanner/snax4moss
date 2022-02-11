33fdb1935c8fd3b31115331acf53af462bd5b605
altanner <8190834+altanner@users.noreply.github.com>
2022-02-11 17:08:26 +0000

    try except

--- medc.py
+++ medc.py
@@ -36,36 +36,40 @@ df = pd.DataFrame(columns=[
     "Isotope", "Source", "Source T", "Absorber", "A Temp",
     "Isomer Shift", "Quad Split", "Comments", "Refcode"])
 
-with alive_bar(final_page, title="Scraping MEDC...") as bar:
-    while page <= final_page:
-        #~ we'll be paginating on {page}, 1 - 3300
-        url = f"http://159.226.238.174:44333/datasearch.php?page={page}&sql=%20AND%20isocode=%20%27fe7%27"
+try:
+    with alive_bar(final_page, title="Scraping MEDC...") as bar:
+        while page <= final_page:
+            #~ we'll be paginating on {page}, 1 - 3300
+            url = f"http://159.226.238.174:44333/datasearch.php?page={page}&sql=%20AND%20isocode=%20%27fe7%27"
 
-        #~ get the page
-        response = requests.get(url,
-            headers=creds.user_agent,
-            cookies=creds.cookies,
-            auth=HTTPBasicAuth(creds.uname, creds.passw))
+            #~ get the page
+            response = requests.get(url,
+                headers=creds.user_agent,
+                cookies=creds.cookies,
+                auth=HTTPBasicAuth(creds.uname, creds.passw))
 
-        #~ soup the page up
-        soup = BeautifulSoup(response.text, "html.parser")
+            #~ soup the page up
+            soup = BeautifulSoup(response.text, "html.parser")
 
-        #~ get the 2nd table (1st is the left panel)
-        hit_table = soup.find_all("table")[1]
+            #~ get the 2nd table (1st is the left panel)
+            hit_table = soup.find_all("table")[1]
 
-        #~ a tr is a row, a td is a cell (ignore header line)
-        for tr in hit_table.find_all("tr")[1:]:
-            tds = tr.find_all("td")
-            this_row = []
-            for cell in tds: #? why can I never get BS to strip tags?
-                #? seriously, tried everything. all links purple. ffs regexing. I know.
-                cell = re.sub("<.?td>", "", str(cell))
-                this_row.append(cell)
+            #~ a tr is a row, a td is a cell (ignore header line)
+            for tr in hit_table.find_all("tr")[1:]:
+                tds = tr.find_all("td")
+                this_row = []
+                for cell in tds: #? why can I never get BS to strip tags?
+                    #? seriously, tried everything. all links purple. ffs regexing. I know.
+                    cell = re.sub("<.?td>", "", str(cell))
+                    this_row.append(cell)
 
-            df.loc[len(df.index)] = this_row
+                df.loc[len(df.index)] = this_row
 
-        page += 1
-        bar()
+            page += 1
+            bar()
+except Exception as e:
+    df.to_csv("./medc_output/medc.csv")
+    print(f"Something went wrong: {e}")
 
 df.to_csv("./medc_output/medc.csv")
 
